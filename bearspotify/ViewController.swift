d751d15014c0170fa5a679559af2c5ad52ffd149
Misael Pérez Chamorro <misaelpcgm@gmail.com>
2018-04-08 15:02:30 -0500

    Initial Commit

---
+++ bearspotify/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  bearspotify
+//
+//  Created by Misael Pérez Chamorro on 4/8/18.
+//  Copyright © 2018 Misael Pérez Chamorro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
