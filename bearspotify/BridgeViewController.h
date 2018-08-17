23ee7e20ffa4cafea149f3db24e8c3510c980ba8
Misael Pérez Chamorro <misaelperezchamorro@Misaels-MacBook-Pro.local>
2018-08-17 18:55:43 -0500

    first commit

---
+++ bearspotify/BridgeViewController.h
@@ -0,0 +1,13 @@
+//
+//  BridgeViewController.h
+//  bearspotify
+//
+//  Created by Misael Pérez Chamorro on 4/8/18.
+//  Copyright © 2018 Misael Pérez Chamorro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BridgeViewController : UIViewController
+
+@end
--- bearspotify/ViewController.swift
+++ bearspotify/ViewController.swift
@@ -8,17 +8,84 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+class ViewController: UIViewController, SPTAudioStreamingPlaybackDelegate, SPTAudioStreamingDelegate {
+  
+  var auth = SPTAuth.defaultInstance()!
+  var session:SPTSession!
+  var player: SPTAudioStreamingController?
+  var loginUrl: URL?
+  let nc = NotificationCenter.default
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    setup()
+     // Note that default is now a property, not a method call
+    nc.addObserver(forName:Notification.Name(rawValue:"loginSuccessfull"),
+                   object:nil, queue:nil) {
+                    notification in
+                    self.updateAfterFirstLogin()
+                    // Handle notification
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    //NotificationCenter.default.addObserver(self, selector: #selector(ViewController.updateAfterFirstLogin), name: NSNotification.Name(rawValue: "loginSuccessfull"), object: self)
+    
+    //NotificationCenter.default.addObserver(self, selector: #selector(ViewController.updateAfterFirstLogin)
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  func setup () {
+    SPTAuth.defaultInstance().clientID = "813e9ee658044c30bbdbc78937fcd878"
+    SPTAuth.defaultInstance().redirectURL = URL(string: "bearspotify://returnAfterLogin")
+    
+    SPTAuth.defaultInstance().tokenSwapURL = URL(string: "bearspotify://returnAfterLogin")
+    
+    SPTAuth.defaultInstance().requestedScopes = [SPTAuthStreamingScope, SPTAuthPlaylistModifyPrivateScope, SPTAuthPlaylistModifyPrivateScope]
+    loginUrl = SPTAuth.defaultInstance().spotifyWebAuthenticationURL()
+  }
+  
+  @IBAction func loginButtonWasTouchedUpInside(sender: Any) {
+    if UIApplication.shared.openURL(loginUrl!) {
+      if auth.canHandle(auth.redirectURL) {
+        
+      }
     }
+  }
+  
+  @objc func updateAfterFirstLogin () {
+    let userDefaults = UserDefaults.standard
+    if let sessionObj:AnyObject = userDefaults.object(forKey: "SpotifySession") as AnyObject? {
+      let sessionDataObj = sessionObj as! Data
+      let firstTimeSession = NSKeyedUnarchiver.unarchiveObject(with: sessionDataObj) as! SPTSession
+      self.session = firstTimeSession
+      initializePlayer(authSession: session)
+    }
+  }
+  
+  func initializePlayer(authSession:SPTSession){
+    if self.player == nil {
+      self.player = SPTAudioStreamingController.sharedInstance()
+      self.player!.playbackDelegate = self
+      self.player!.delegate = self
+      try! player!.start(withClientId: auth.clientID)
+      self.player!.login(withAccessToken: authSession.accessToken)
+    }
+  }
+  
+  
+  func audioStreamingDidLogin(_ audioStreaming: SPTAudioStreamingController!) {
+    // after a user authenticates a session, the SPTAudioStreamingController is then initialized and this method called
+    print("logged in")
+    self.player?.playSpotifyURI("spotify:track:58s6EuEYJdlb0kO7awm3Vp", startingWith: 0, startingWithPosition: 0, callback: { (error) in
+      if (error != nil) {
+        print("playing!")
+      }
+    })
+  }
+  
 
 
 }
